127b266f63653c11b4567f32828669bfcb4b5e7c
roorooro1 <roorooro9933@gmail.com>
2021-10-29 20:30:23 +0900

    dummy text

--- main.rb
+++ main.rb
@@ -1,11 +1,24 @@
 
-students = [
-    {
-        "username" => "jinsoo",
-        "password" => "password"
-    }
-]
+# init
 
-puts students
+# import React from 'react'
+# import TodoForm from "./TodoForm"
+# import Todo from "./Todo"
 
-puts students.methods
+# TodoMVC
+# 1. add todos
+# 2. display todos
+# 3. cross off todo
+# 4. show number of active todos
+# 5. filter all/active/complete
+# 6. delete todo
+# 7. delete all complete
+# 8. button to toggle all on/off
+
+# export default calss TodoList extends React.Component{
+    # state = {
+    #     todos:[],
+    #     todo_to_show: "all",
+    #     toggle_all_complete: true
+    # }
+# }
