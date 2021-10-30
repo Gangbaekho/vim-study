eb04fb104b4f2be48aba94aa288948bcc5f725bb
roorooro1 <roorooro9933@gmail.com>
2021-10-30 20:44:28 +0900

    method practice with vim

---
+++ methods.rb
@@ -0,0 +1,18 @@
+
+def multifly(first_num, second_num)
+    first_num.to_i * second_num.to_i
+end
+
+def add(first_num, second_num)
+    first_num.to_i + second_num.to_i
+end
+
+def substract(first_num, second_num)
+    first_num.to_i - second_num.to_i
+end
+
+puts multifly("1","4")
+
+puts add(2,19)
+
+puts substract(19,3)
